1f393b80e4ab92ad2b2b1d2b146d114204ad14b5
Mandy Xiong <xion0016@algonquinlive.com>
2017-05-15 12:17:44 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var a = 3;
+var b = 5;
+var c;
+
+alert( "var a = 3;\nvar b = 5;\nvar c;\n___________________\n" + 
+      "a + b = " + (a+b) + "\n" + 
+      "a - b = " + (a-b) + "\n" + 
+      "a * b = " + (a*b) + "\n" + 
+      "a % b = " + (a%b) + "\n" + 
+      "a += b = " + (a+=b) + "\n" +
+      "a *= b = " + (a*=b) + "\n" +
+      "a /= b = " + (a/=b) + "\n" +
+      "a %= b = " + (a%=b) + "\n" +
+      "a == b = " + (a==b) + "\n" +
+      "a != b = " + (a!=b) + "\n" +
+      "a > b = " + (a>b) + "\n" + 
+      "a < b = " + (a<b) + "\n" + 
+      "!a && !c  = " + (!a&&!c) + "\n" + 
+      "!a || !c = " + (!a||!c) + "\n"
+     );
+
+
+var first_name;
+first_name = "Mandy";
+
+var last_name ;
+last_name = "Xiong";
+
+var email ;
+email = "xiongmanyi@gmail.com";
+
+var output ;
+
+alert ("My name is " + (first_name) + " " + (last_name) + "." + " You can contact me at " + (email) + ".");
